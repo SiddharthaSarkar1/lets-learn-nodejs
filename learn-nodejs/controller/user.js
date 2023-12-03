0e5396699511cd0d29b6e8954f5c854437794b06
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-12-03 22:12:25 +0530

    user Model added and user Controller is updated

--- learn-nodejs/controller/user.js
+++ learn-nodejs/controller/user.js
@@ -1,45 +1,78 @@
 const fs = require('fs');
 // const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-const users = data.users;
+// const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+// const users = data.users;
+
+const model = require("../model/user");
+const User = model.User;
 
 //MVC model-view-controller
-exports.createUser = (req, res) => {
-    const newUser = req.body;
-    users.push(newUser);
-    res.status(201).json(newUser);
-    //res.json({Message: 'Successfully added a new user.'});
+exports.createUser = async (req, res) => {
+
+    try {
+        const user = new User(req.body);
+        const output = await user.save();
+        console.log(output);
+        return res
+          .status(200)
+          .json({ Message: "Successfully added a new user." });
+      } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+      }
 }
 
-exports.getAllUsers = (req, res) => {
-    res.status(200).json(users);
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+      } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+      }
 }
 
-exports.getUser = (req, res) => {
-    const id = +req.params.id; //Converting the string id to numeric id
-    const user = users.find((user) => user.id == id )
-    res.status(200).json(user);
+exports.getUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+      } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+      }
+}
+// PUT request http://localhost:8800/users/656cad361a311d9318ffd41a
+exports.replaceUser = async (req, res) => {
+    try {
+        const id = req.params.id; 
+        const result = await User.findOneAndReplace({ _id: id }, req.body, {new: true});
+        return res.status(201).json(result);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
 }
 
-exports.replaceUser = (req, res) => {
-    const id = +req.params.id; //Converting the string id to numeric id
-    const userIndex = users.findIndex((user) => user.id === id );
-    users.splice(userIndex, 1, {...req.body, id: id});
-    res.status(201).json({ Message: "user is updated successfully."});
+// PATCH request http://localhost:8800/users/656cad361a311d9318ffd41a
+exports.updateUser = async (req, res) => {
+    try {
+        const id = req.params.id; 
+        const result = await User.findOneAndUpdate({ _id: id }, req.body, {new: true});
+        return res.status(201).json(result);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
 }
 
-exports.updateUser = (req, res) => {
-    const id = +req.params.id; //Converting the string id to numeric id
-    const userIndex = users.findIndex((user) => user.id === id );
-    const user = users[userIndex];
-    users.splice(userIndex, 1, {...user, ...req.body});
-    res.status(201).json({ Message: `user at index ${userIndex + 1} is updated successfully.`});
-}
-
-exports.deleteUser = (req, res) => {
-    const id = +req.params.id; //Converting the string id to numeric id
-    const userIndex = users.findIndex((user) => user.id === id );
-    const user = users[userIndex];
-    users.splice(userIndex, 1);
-    res.status(200).json(user);
+exports.deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id; 
+        const result = await User.findOneAndDelete({ _id: id });
+        return res.status(201).json(result);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
 }
